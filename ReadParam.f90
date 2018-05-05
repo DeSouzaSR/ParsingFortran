58414c4985dedae227c8ecd69512459752c1b228
DeSouzaSR <sandro.fisica@gmail.com>
2018-05-05 10:26:52 -0300

    Create ReadParam.

---
+++ ReadParam.f90
@@ -0,0 +1,34 @@
+program inputs
+    !--------------------------------------------------------------------------
+    ! Program, in Fortran 90, to read parameters in a file
+    !   excluding one or more data
+    ! 
+    ! Created by Sandro R. DeSouza <sandro.fisica@gmail.com>
+    ! Date: 2018-05-05
+    !--------------------------------------------------------------------------
+    
+    ! Declare variable and parameters
+    implicit none
+    integer :: u ! unit
+    integer :: j ! iteract
+    character(len=30) :: label ! To ignore variable label
+    real :: a, b, c, d
+    real, dimension(3) :: vec1
+	
+    ! Read parameters 
+    open(unit = u, file = "param.dat", status = "old")
+    read(u,*) label, a
+    read(u,*) label, b
+    read(u,*) label, (vec1(j), j=1, 3)
+    read(u,*) ! ignoring data
+    read(u,*) label, c
+    close(u) ! Close file param.dat
+
+    ! Compute d
+    d = a + b + c
+
+    ! Write out results
+    write(*,*) "e : ", d
+    write(*,*) "Vector: "
+    write(*,*) (vec1(j), j=1,3)
+end program inputs
